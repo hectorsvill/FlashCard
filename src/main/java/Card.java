431cc1cc054a4395f5a66bc6c65db22f8bd19ee7
Hector Steven Villasano <hectorstevenvillasano@gmail.com>
2022-04-22 06:17:03 -0700

    Card object

---
+++ src/main/java/Card.java
@@ -0,0 +1,9 @@
+public class Card {
+    String question;
+    String solution;
+
+    public Card(String question, String solution){
+        this.question = question;
+        this.solution = solution;
+    }
+}
